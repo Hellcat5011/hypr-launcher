9268f092977caf2b987178da80cd9ffdb0d77e86
arcolinuxz <arcolinuxinfo@gmail.com>
2026-09-23 22:52:26 +0530

    animations update

--- configs/hypr/user/animations.lua
+++ configs/hypr/user/animations.lua
@@ -1,29 +1,59 @@
--- Default curves and animations, see https://wiki.hypr.land/Configuring/Advanced-and-Cool/Animations/
-hl.curve("easeOutQuint",   { type = "bezier", points = { {0.23, 1},    {0.32, 1}    } })
-hl.curve("easeInOutCubic", { type = "bezier", points = { {0.65, 0.05}, {0.36, 1}    } })
-hl.curve("linear",         { type = "bezier", points = { {0, 0},       {1, 1}       } })
-hl.curve("almostLinear",   { type = "bezier", points = { {0.5, 0.5},   {0.75, 1}    } })
-hl.curve("quick",          { type = "bezier", points = { {0.15, 0},    {0.1, 1}     } })
+-- Global speed multiplier: 1.0 = as designed, <1 faster, >1 slower
+local duration_scale = 1.3
 
--- Default springs
-hl.curve("easy",           { type = "spring", mass = 1, stiffness = 238.1191, dampening = 24.21279333 })
-
-hl.animation({ leaf = "global",        enabled = true,  speed = 10,   bezier = "default" })
-hl.animation({ leaf = "border",        enabled = true,  speed = 5.39, bezier = "easeOutQuint" })
-hl.animation({ leaf = "windows",       enabled = true,  speed = 4.79, spring = "easy" })
-hl.animation({ leaf = "windowsIn",     enabled = true,  speed = 4.1,  spring = "easy",         style = "popin 87%" })
-hl.animation({ leaf = "windowsOut",    enabled = true,  speed = 1.49, bezier = "linear",       style = "popin 87%" })
-hl.animation({ leaf = "fadeIn",        enabled = true,  speed = 1.73, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeOut",       enabled = true,  speed = 1.46, bezier = "almostLinear" })
-hl.animation({ leaf = "fade",          enabled = true,  speed = 3.03, bezier = "quick" })
-hl.animation({ leaf = "layers",        enabled = true,  speed = 3.81, bezier = "easeOutQuint" })
-hl.animation({ leaf = "layersIn",      enabled = true,  speed = 4,    bezier = "easeOutQuint", style = "fade" })
-hl.animation({ leaf = "layersOut",     enabled = true,  speed = 1.5,  bezier = "linear",       style = "fade" })
-hl.animation({ leaf = "fadeLayersIn",  enabled = true,  speed = 1.79, bezier = "almostLinear" })
-hl.animation({ leaf = "fadeLayersOut", enabled = true,  speed = 1.39, bezier = "almostLinear" })
-hl.animation({ leaf = "workspaces",    enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesIn",  enabled = true,  speed = 1.21, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "workspacesOut", enabled = true,  speed = 1.94, bezier = "almostLinear", style = "fade" })
-hl.animation({ leaf = "zoomFactor",    enabled = true,  speed = 7,    bezier = "quick" })
+local prod = function(ds)
+    return ds * duration_scale
+end
 
 
+
+hl.curve("wind", {type = "bezier", points = {{0.05, 0.85}, {0.03, 0.97}}})
+hl.curve("winIn", {type = "bezier", points = {{0.07, 0.88}, {0.04, 0.99}}})
+hl.curve("winOut", {type = "bezier", points = {{0.20, -0.15}, {0, 1}}})
+hl.curve("liner", {type = "bezier", points = {{1, 1}, {1, 1}}})
+hl.curve("md3_standard", {type = "bezier", points = {{0.12, 0}, {0, 1}}})
+hl.curve("md3_decel", {type = "bezier", points = {{0.05, 0.80}, {0.10, 0.97}}})
+hl.curve("md3_accel", {type = "bezier", points = {{0.20, 0}, {0.80, 0.08}}})
+hl.curve("overshot", {type = "bezier", points = {{0.05, 0.85}, {0.07, 1.04}}})
+hl.curve("crazyshot", {type = "bezier", points = {{0.1, 1.22}, {0.68, 0.98}}})
+hl.curve("hyprnostretch", {type = "bezier", points = {{0.05, 0.82}, {0.03, 0.94}}})
+hl.curve("menu_decel", {type = "bezier", points = {{0.05, 0.82}, {0, 1}}})
+hl.curve("menu_accel", {type = "bezier", points = {{0.20, 0}, {0.82, 0.10}}})
+hl.curve("easeInOutCirc", {type = "bezier", points = {{0.75, 0}, {0.15, 1}}})
+hl.curve("easeOutCirc", {type = "bezier", points = {{0, 0.48}, {0.38, 1}}})
+hl.curve("easeOutExpo", {type = "bezier", points = {{0.10, 0.94}, {0.23, 0.98}}})
+hl.curve("softAcDecel", {type = "bezier", points = {{0.20, 0.20}, {0.15, 1}}})
+hl.curve("md2", {type = "bezier", points = {{0.30, 0}, {0.15, 1}}})
+hl.curve("OutBack", {type = "bezier", points = {{0.28, 1.40}, {0.58, 1}}})
+hl.animation({leaf = "fadeIn", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeOut", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeSwitch", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeShadow", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeDim", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeLayers", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadePopups", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadeDpms", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "workspacesIn", enabled = true, speed = prod(4.0), bezier = "menu_decel", style = "slidevert"})
+hl.animation({leaf = "workspacesOut", enabled = true, speed = prod(4.0), bezier = "menu_decel", style = "slidevert"})
+hl.animation(
+    {leaf = "specialWorkspaceIn", enabled = true, speed = prod(2.3), bezier = "md3_decel", style = "slidefadevert 15%"}
+)
+hl.animation(
+    {leaf = "specialWorkspaceOut", enabled = true, speed = prod(2.3), bezier = "md3_decel", style = "slidefadevert 15%"}
+)
+hl.animation({leaf = "border", enabled = true, speed = prod(1.6), bezier = "liner"})
+hl.animation({leaf = "borderangle", enabled = false, speed = prod(82), bezier = "liner", style = "loop"})
+hl.animation({leaf = "windowsIn", enabled = true, speed = prod(3.8), bezier = "winIn", style = "slide"})
+hl.animation({leaf = "windowsOut", enabled = true, speed = prod(2.7), bezier = "winOut"})
+hl.animation({leaf = "windowsMove", enabled = true, speed = prod(3.0), bezier = "wind", style = "slide"})
+hl.animation({leaf = "fade", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "layersIn", enabled = true, speed = prod(1.8), bezier = "menu_decel", style = "slide"})
+hl.animation({leaf = "layersOut", enabled = true, speed = prod(1.5), bezier = "menu_accel"})
+hl.animation({leaf = "fadeLayersIn", enabled = true, speed = prod(1.6), bezier = "menu_decel"})
+hl.animation({leaf = "fadeLayersOut", enabled = true, speed = prod(1.8), bezier = "menu_accel"})
+hl.animation({leaf = "workspaces", enabled = true, speed = prod(4.0), bezier = "menu_decel", style = "slidevert"})
+hl.animation(
+    {leaf = "specialWorkspace", enabled = true, speed = prod(2.3), bezier = "md3_decel", style = "slidefadevert 15%"}
+)
+hl.animation({leaf = "fadePopupsIn", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
+hl.animation({leaf = "fadePopupsOut", enabled = true, speed = prod(1.8), bezier = "md3_decel"})
